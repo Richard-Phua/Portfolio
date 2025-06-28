ad0f1d3d91eb1c0f39b6300ed987f280dd790c42
Richard-Phua <90612480+Richard-Phua@users.noreply.github.com>
2025-06-28 18:04:32 +0800

    Delete Portfolio directory

--- Portfolio/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("myh1").textContent = 'Hello';
-
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
